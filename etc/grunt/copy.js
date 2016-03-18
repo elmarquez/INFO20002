a690ca17e006dc514520848c7a8e1edec2917b97
Davis Marques <dmarques@unimelb.edu.au>
2016-03-18 13:06:23 +1100

    Modify layout of dist folder to work better on server.

--- etc/grunt/copy.js
+++ etc/grunt/copy.js
@@ -1,10 +1,40 @@
 'use strict';
 
 module.exports = {
-  src: {
-    cwd: 'src',
+  srcCss: {
+    cwd: 'src/css',
     src: [ '**/*' ],
-    dest: 'dist/',
+    dest: 'dist/css',
+    expand: true
+  },
+  srcHtml: {
+    cwd: 'src/html',
+    src: [ '**/*' ],
+    dest: 'dist/html',
+    expand: true
+  },
+  stylesCss: {
+    cwd: 'src',
+    src: [ 'styles.css' ],
+    dest: 'dist/css',
+    expand: true
+  },
+  stylesHtml: {
+    cwd: 'src',
+    src: [ 'styles.css' ],
+    dest: 'dist/html',
+    expand: true
+  },
+  vendorCss: {
+    cwd: 'vendor',
+    src: [ '**/*' ],
+    dest: 'dist/css',
+    expand: true
+  },
+  vendorHtml: {
+    cwd: 'vendor',
+    src: [ '**/*' ],
+    dest: 'dist/html',
     expand: true
   }
 };
